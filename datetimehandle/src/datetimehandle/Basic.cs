a577e1754c438500a6a39dec44817b8925d3cb25
Manoj Bhargavan <manojbhargavan07@gmail.com>
2019-07-27 18:15:26 +0530

    tz

--- datetimehandle/src/datetimehandle/Basic.cs
+++ datetimehandle/src/datetimehandle/Basic.cs
@@ -1,9 +1,22 @@
+using System;
+
 namespace datetimehandle
 {
     public static class Basic
     {
-        public static void GetTimeGivenTimeZone()
+        public static DateTime GetDateTimeNowGivenTimeZone(DateTime dateTime, string timeZone)
         {
+            TimeZoneInfo timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById(timeZone);
+            DateTime timeZonesDateTime;
+            try
+            {
+                timeZonesDateTime = TimeZoneInfo.ConvertTime(dateTime, timeZoneInfo);
+                return timeZonesDateTime;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
 
         }
 
