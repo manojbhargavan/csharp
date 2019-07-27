5e1840b4086e47bd73e9799297f801db185694c8
Manoj Bhargavan <manojbhargavan07@gmail.com>
2019-07-27 20:08:52 +0530

    Basics done

--- datetimehandle/src/datetimehandle/Basic.cs
+++ datetimehandle/src/datetimehandle/Basic.cs
@@ -1,23 +1,34 @@
 using System;
+using System.Collections.Generic;
 
 namespace datetimehandle
 {
     public static class Basic
     {
-        public static DateTime GetDateTimeNowGivenTimeZone(DateTime dateTime, string timeZone)
+        public static DateTime GetConverted(this DateTime dateTime, string timeZone)
         {
             TimeZoneInfo timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById(timeZone);
-            DateTime timeZonesDateTime;
             try
             {
-                timeZonesDateTime = TimeZoneInfo.ConvertTime(dateTime, timeZoneInfo);
-                return timeZonesDateTime;
+                return TimeZoneInfo.ConvertTime(dateTime, timeZoneInfo);
             }
             catch (Exception ex)
             {
                 throw ex;
             }
+        }
 
+        public static List<string> GetTimeZone(this DateTimeOffset dateTime)
+        {
+            List<string> timeZoneDisplayName = new List<string>();
+            foreach (var timeZone in TimeZoneInfo.GetSystemTimeZones())
+            {
+                if(timeZone.GetUtcOffset(dateTime) == dateTime.Offset)
+                {
+                    timeZoneDisplayName.Add(timeZone.DisplayName);
+                }
+            }
+            return timeZoneDisplayName;
         }
 
     }
--- datetimehandle/src/datetimehandle/Program.cs
+++ datetimehandle/src/datetimehandle/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using System.Linq;
 
 namespace datetimehandle
 {
@@ -6,7 +8,39 @@ namespace datetimehandle
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Basics();
+        }
+
+        private static void Basics()
+        {
+            var dateTime = DateTime.Now;
+            Console.WriteLine(
+                $"Local: {dateTime}, " +
+                $"AUS: {dateTime.GetConverted("E. Australia Standard Time")}\n");
+
+            //Date Time Offset
+            var dateTimeOffset = DateTimeOffset.Now;
+            Console.WriteLine($"Local: {dateTimeOffset}\nTimeZones Matching Offset:\n{DateTimeOffset.Now.GetTimeZone().Aggregate((a, b) => $"{a}\n{b}") ?? "Not Found"}\n");
+            var dateTimeOffset5 = dateTimeOffset.ToOffset(TimeSpan.FromHours(10));
+            Console.WriteLine($"UTC+10: {dateTimeOffset5}\nTimeZones Matching Offset:\n{dateTimeOffset5.GetTimeZone().Aggregate((a, b) => $"{a}\n{b}") ?? "Not Found"}\n");
+            var dateTimeOffset2 = dateTimeOffset.ToOffset(TimeSpan.FromHours(2));
+            Console.WriteLine($"UTC+2: {dateTimeOffset2}\nTimeZones Matching Offset:\n{dateTimeOffset2.GetTimeZone().Aggregate((a, b) => $"{a}\n{b}") ?? "Not Found"}\n");
+
+            //Parse
+            var date = "9/10/2019 10:00:00 PM";
+            Console.WriteLine($"Date String: {date}");
+            Console.WriteLine($"Parse :{DateTime.Parse(date):dd MMM yyyy hh:mm:tt}");
+            Console.WriteLine($"Parse Exact :{DateTime.ParseExact(date, "d/M/yyyy h:mm:ss tt", CultureInfo.InvariantCulture):dd MMM yyyy hh:mm:tt}");
+
+            var dateVar = "2019-07-01 10:00:00 PM +02:00";
+            Console.WriteLine($"Date String: {dateVar}");
+            Console.WriteLine($"Parse :{DateTime.Parse(dateVar, CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal)}");
+
+            //https://docs.microsoft.com/en-us/dotnet/standard/base-types/standard-date-and-time-format-strings
+
+            //Always use DateTimeOffset & UTCNow
+            var dateTimeUtc = DateTimeOffset.UtcNow;
+            Console.WriteLine($"\nUTC: {dateTimeUtc:o}, Local: {dateTimeUtc.ToLocalTime():o}");
         }
     }
 }
