8569d61003a55ccf9bee6f6b3f254c2733ae34fb
Manoj Bhargavan <manojbhargavan07@gmail.com>
2019-07-28 20:20:47 +0530

    Interface: Explicit IEnumerable Implementation

---
+++ Interfaces/ExplicitImplementation/ExplicitImplementation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExplicitImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Odd Numbers: ");
+            var generator = new OddNumberGenerator();
+            foreach(var odd in generator)
+            {
+                if (odd > 50)
+                    break;
+                Console.Write(odd + " ");
+            }
+        }
+    }
+}
