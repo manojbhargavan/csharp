d6a3d32c75b800bc973adc64313cafdf1f3e03c3
Manoj Bhargavan <manojbhargavan07@gmail.com>
2019-07-28 14:29:24 +0530

    Added Some Interface Example

---
+++ Interfaces/FutureProofing/PeopleViewer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PeopleViewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
