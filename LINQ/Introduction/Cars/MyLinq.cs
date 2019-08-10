91fbe47c98f236ad835720edc44fe4ed79872787
Manoj Bhargavan <manojbhargavan07@gmail.com>
2019-08-11 00:02:13 +0530

    Going go

---
+++ LINQ/Introduction/Cars/MyLinq.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public static class MyLinq
+    {
+        public static void Print<T>(this IEnumerable<T> source)
+        {
+            foreach (var item in source)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
--- LINQ/Introduction/Cars/Program.cs
+++ LINQ/Introduction/Cars/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Cars
 {
@@ -6,7 +9,32 @@ namespace Cars
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var cars = ProcessFile("fuel.csv");
+            //cars.Print();
+            Console.WriteLine("***\nMost Efficient Cars");
+            //Fuel Eff Car
+            cars.OrderByDescending(c => c.Combined).ThenBy(c => c.Name).Take(10).Print();
+            Console.WriteLine("***\nLeast Efficient Cars");
+            (from car in cars orderby car.Combined, car.Name select car).Take(10).Print();
+            Console.WriteLine("***\nMost Efficient Cars - BMW 2016");
+            cars.Where(c => c.Manufacturer.ToUpper() == "BMW" && c.Year == 2016)
+                .OrderByDescending(c => c.Combined)
+                .ThenBy(c => c.Name)
+                .Take(10).Print();
+        }
+
+        private static List<Car> ProcessFile(string path)
+        {
+            //return File.ReadAllLines(path)
+            //    .Skip(1)
+            //    .Where(line => line.Length > 1)
+            //    .Select(Car.ParseFromCsv)
+            //    .ToList();
+
+            return (from line in File.ReadAllLines(path).Skip(1)
+                    where line.Length >= 1
+                    select Car.ParseFromCsv(line)).ToList();
+
         }
     }
 }
