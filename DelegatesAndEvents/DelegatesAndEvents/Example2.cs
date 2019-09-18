08e53ffedb17f5114710e7a874f12aeb96d413f5
Manoj Bhargavan <manojbhargavan07@gmail.com>
2019-09-18 22:54:10 +0530

    One example added

---
+++ DelegatesAndEvents/DelegatesAndEvents/Example2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvents
+{
+    public delegate void WhatAreYourWorkingOn(int hours);
+
+    public class Example2 : IExample
+    {
+        public void RunExample()
+        {
+            WhatAreYourWorkingOn whatAreYourWorkingOnDelegate = Coding;
+            whatAreYourWorkingOnDelegate += UTC;
+            whatAreYourWorkingOnDelegate += QA;
+            whatAreYourWorkingOnDelegate(8);
+        }
+
+        private void Coding(int hrs)
+        {
+            Console.WriteLine($"I am {WorkType.Code}ing its about {hrs} hours");
+        }
+
+        private void UTC(int hrs)
+        {
+            Console.WriteLine($"I am {WorkType.UnitTest}ing its about {hrs} hours");
+        }
+
+        private void QA(int hrs)
+        {
+            Console.WriteLine($"{WorkType.QA} is about to start in {hrs} hours");
+        }
+    }
+}
--- DelegatesAndEvents/DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/DelegatesAndEvents/Program.cs
@@ -7,6 +7,8 @@ namespace DelegatesAndEvents
         static void Main(string[] args)
         {
             RunExamples(typeof(Example1));
+            Console.WriteLine("*******************");
+            RunExamples(typeof(Example2));
         }
 
         internal static void RunExamples(Type example)
