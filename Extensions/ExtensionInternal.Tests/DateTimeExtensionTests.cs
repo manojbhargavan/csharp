a7cf8fb99c3810e4af41e77122abcf5ec351637c
Manoj Bhargavan <manojbhargavan07@gmail.com>
2019-09-27 23:42:55 +0530

    dt

--- Extensions/ExtensionInternal.Tests/DateTimeExtensionTests.cs
+++ Extensions/ExtensionInternal.Tests/DateTimeExtensionTests.cs
@@ -8,8 +8,9 @@ namespace ExtensionInternal.Tests
         [Fact]
         public void StaticCallVsExtensionCall()
         {
-            string result1 = DateTimeExtension.ToXmlDateTimeString(new DateTime(2019, 12, 21));
-            string result2 = (new DateTime(2019, 12, 21)).ToXmlDateTimeString();
+            var dateTime = new DateTime(2019, 12, 21);
+            string result1 = DateTimeExtension.ToXmlDateTimeString(dateTime);
+            string result2 = dateTime.ToXmlDateTimeString();
             Assert.Equal(result1, result2);
         }
     }
